a68d5c3c8b02c34065b2ec16ad752f5019823578
kampytaru <matsumi@gmail.com>
2022-04-12 02:46:35 -0700

    Added submit confirmation

--- src/app/pages/admin/clip-viewer/clip-viewer.component.ts
+++ src/app/pages/admin/clip-viewer/clip-viewer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { take } from 'rxjs/operators';
 import { TwitchGetClipData, TwitchGetClipsResponse } from 'src/app/interfaces/twitch.interface';
 import { HttpService } from 'src/app/services/http/http.service';
+import { TokenService } from 'src/app/services/token/token.service';
 
 @Component({
   selector: 'app-clip-viewer',
@@ -10,11 +11,14 @@ import { HttpService } from 'src/app/services/http/http.service';
 })
 export class ClipViewerComponent implements OnInit {
   clips?: TwitchGetClipsResponse;
+  submitted = false;
   added: TwitchGetClipData[] = [];
+  streamer?: string;
 
-  constructor(private http: HttpService) { }
+  constructor(private http: HttpService, private token: TokenService) { }
 
   ngOnInit(): void {
+    this.streamer = this.token.getTokenFromSession().login;
     this.http.get('/clip-viewer').pipe(
       take(1)
     ).subscribe(clips => {
@@ -23,6 +27,7 @@ export class ClipViewerComponent implements OnInit {
   }
 
   add(clip: TwitchGetClipData): void {
+    this.submitted = false;
     if (!this.added.includes(clip)) {
       this.added.push(clip);
     }
@@ -30,6 +35,7 @@ export class ClipViewerComponent implements OnInit {
 
   submit(): void {
     const submitValue = this.added.map(clip => {
+      this.submitted = true;
       return {
         streamerName: clip.broadcaster_name,
         creatorName: clip.creator_name,
