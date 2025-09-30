311fdbc6a331807d55756cada66347ed67ef7e5d
asarimitsuha <asari@angelssword.com>
2025-09-29 21:36:32 -0500

    updated button add tool

--- src/app/pages/websource/subathon/button/button.component.ts
+++ src/app/pages/websource/subathon/button/button.component.ts
@@ -1,22 +1,26 @@
-import { HttpSentEvent } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpService } from 'src/app/services/http/http.service';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
-  constructor(private http: HttpService) { }
+  form = this.formBuilder.group({
+    kick: 1,
+    twitch: 1
+  });
+  constructor(private http: HttpService,  private formBuilder: FormBuilder) { }
 
   addKick(): void {
-    this.http.get('/subathon/kick').subscribe(() => {});
+    this.http.post('/subathon/kick', { count: this.form.controls.kick }).subscribe(() => {});
   }
 
   addTwitch(): void {
-    this.http.get('/subathon/twitch').subscribe(() => {});
+    this.http.post('/subathon/twitch/add', { count: this.form.controls.twitch }).subscribe(() => {});
   }
 }
